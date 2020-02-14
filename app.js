4141bf04aa1d3b1b90759b2155df71da874a9af5
ORIFJON NARKULOV <orifjon9@gmail.com>
2020-02-14 07:24:54 -0700

    add Expressjs

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+    console.log('In middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    next();
+});
+
+app.use('/products', (req, res, next) => {
+    res.send('<h1>Products page</h1>')
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>')
+});
 
 
-server.listen(3000, () => {
+app.listen(3000, () => {
     console.log('http://localhost:3000 started');
 })
