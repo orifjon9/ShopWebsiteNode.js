e6a9550f05fee119e6b008a143f64434f6faa7ef
ORIFJON NARKULOV <orifjon9@gmail.com>
2020-02-18 06:32:07 -0700

    Add HTML pages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const path = require('./util/path');
 
 const app = express();
 
@@ -8,11 +8,13 @@ const adminRouters = require('./routes/admin');
 const shopRouters = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path('public')));
+
 app.use('/admin', adminRouters);
 app.use(shopRouters);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path('views', '404.html'));
 })
 
 app.listen(3000, () => {
