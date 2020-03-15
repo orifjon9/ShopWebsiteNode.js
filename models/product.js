a93fe1e1c0994524c0a45bfd3031ca6d047e72dc
ORIFJON NARKULOV <orifjon9@gmail.com>
2020-03-15 07:32:44 -0600

    SQL approach to persist data in mysql

--- models/product.js
+++ models/product.js
@@ -1,79 +1,51 @@
 const fs = require('fs');
 
-const path = require('../util/path');
-const p = path('data', 'products.json');
+const db = require('../util/database');
 const Cart = require('../models/cart');
 
-
-const getProductsFromFile = callback => {
-    fs.readFile(p, (err, fileContent) => {
-        if (!err) {
-            return callback(JSON.parse(fileContent));
-        }
-        callback([]);
-    });
-}
-
-const saveProductsToFile = (products, callback) => {
-    fs.writeFile(p, JSON.stringify(products), err => callback(err));
-};
-
 module.exports = class Product {
-    constructor(title, imageUrl, price, description) {
+    constructor(title, imageUrl, price, description, id) {
         this.title = title;
         this.imageUrl = imageUrl;
         this.price = +price;
         this.description = description;
+        this.id = id;
     }
 
     save = () => {
-        getProductsFromFile(products => {
-            this.id = this.getNextId(products);
-            products.push(this);
-            saveProductsToFile(products, err => {
-                console.log(err);
-            });
-        });
+        db.execute('INSERT INTO products(title, description, price, imageUrl) VALUES (?, ?, ?, ?)',
+            [this.title, this.description, this.price, this.imageUrl])
+            .catch(err => console.log(err));
     }
 
     update = () => {
-        getProductsFromFile(products => {
-            const productIndex = products.findIndex(p => p.id === this.id);
-            console.log(productIndex);
-            if (productIndex > -1) {
-                let updatedProducts = [...products];
-                updatedProducts[productIndex] = this;
-                saveProductsToFile(updatedProducts, err => {
-                    console.log(err);
-                });
-            }
-        });
+        db.execute('UPDATE products SET title = ?, description = ?, price = ?, imageUrl = ? WHERE id = ?',
+            [this.title, this.description, this.price, this.imageUrl, this.id])
+            .catch(err => console.log(err));
     }
 
     static deleteById = (id) => {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id === id);
-            const updatedProducts = products.filter(p => p.id !== id);
-            saveProductsToFile(updatedProducts, err => {
-                if (!err) {
-                    Cart.deleteProduct(id, product.price);
-                }
-            });
-        });
+        db.execute('DELETE products FROM WHERE id = ? ', [id])
+            .then(() => {
+                // TODO:
+                // Cart.deleteProduct(id, product.price);
+            })
+            .catch(err => console.log(err));
     }
 
-    getNextId = products => Math.max.apply(Math, products.map(p => p.id)) + 1 || 1;
-
-
-
     static fetchAll = (callback) => {
-        getProductsFromFile(callback);
+        db.execute('SELECT * FROM products')
+            .then(([rows, fields]) => {
+                callback(rows);
+            })
+            .catch(err => console.log(err));
     }
 
     static getById = (id, callback) => {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id === +id);
-            callback(product);
-        });
+        db.execute('SELECT * FROM products WHERE id = ? ', [id])
+            .then(([rows, fields]) => {
+                callback(rows[0]);
+            })
+            .catch(err => console.log(err));
     };
 }
